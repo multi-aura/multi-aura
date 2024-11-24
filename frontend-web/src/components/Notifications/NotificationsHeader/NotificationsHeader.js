17a4e3761a17d678ff040f0f28cf4d6d749eab63
HTrongThang <trongthang412003@gmail.com>
2024-11-24 11:43:47 +0700

    feat: implemented design web notification

---
+++ frontend-web/src/components/Notifications/NotificationsHeader/NotificationsHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NotificationsHeader.css";
+
+const NotificationsHeader = () => {
+  return (
+    <div className="drawer-header">
+      <h2 className="header-title-contification">Thông báo</h2>
+    </div>
+  );
+};
+
+export default NotificationsHeader;
